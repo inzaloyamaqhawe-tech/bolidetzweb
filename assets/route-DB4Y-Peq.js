df9bc0bbc351def11000a7ad3a8a7b9ccc12b081
inzaloyamaqhawe-tech <inzaloyamaqhawe-tech@github.com>
2026-09-04 09:20:34 +0200

    Replace Bolide Secure hero image with the new mall/CCTV photo

---
+++ assets/route-DB4Y-Peq.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-CY_MMzgk.js";import{D as t}from"./index-DmRKP8yl.js";var n=e(),r=()=>(0,n.jsx)(t,{});export{r as component};
--- assets/route-NfV8VFPd.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-CY_MMzgk.js";import{D as t}from"./index-Bo-NFXS4.js";var n=e(),r=()=>(0,n.jsx)(t,{});export{r as component};
